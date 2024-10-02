4cad40b4504e307aed709c8c0c47b9b63fb91af8
Blitzer0007 <harishjune777@gmail.com>
2024-10-02 14:37:04 +0530

    Initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,15 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+// example.spec.js
+test('Visual Comparison Test Demo', async ({ page },testinfo) => {
+  //await page.goto('https://superset.datatest.ch/superset/explore/?form_data_key=vP6zgie87qSb4OMCFshsooudGR3C9DHwHr688EE7nMVuDQPxzJkIZM-ZZHPtQHzD&slice_id=133');
+  await page.goto('https://www.chartjs.org/docs/latest/samples/area/line-stacked.html')
+  await page.waitForTimeout(3000)
+  await page.waitForLoadState()
+
+  expect(await page.screenshot()).toMatchSnapshot({ maxDiffPixels: 200 });
+  const screenshot = await page.screenshot();
+  await testinfo.attach('screenshot', { body: screenshot, contentType: 'image/png' });
+
+});
